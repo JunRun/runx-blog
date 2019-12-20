9f058ea5a0436c51169902bd09797148458de853
JUNRUN <934776347@qq.com>
2019-12-20 18:22:07 +0800

    搭建前端项目框架

--- blog-h5/src/main.js
+++ blog-h5/src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
-// import '@/styles/index.scss' // global css
-// import '@/icons' // icon
-// import '@/permission' // permission control
+import '@/styles/index.scss' // global css
+import '@/icons' // icon
+import '@/permission' // permission control
 
 Vue.config.productionTip = false
 
