f41352d020dd30b9a3bea4fe4125cca79c9a7649
JUNRUN <934776347@qq.com>
2019-12-20 17:01:35 +0800

    搭建项目框架

--- blog-h5/src/main.js
+++ blog-h5/src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// import '@/styles/index.scss' // global css
+// import '@/icons' // icon
+// import '@/permission' // permission control
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI, { locale })
 new Vue({
   router,
   store,
